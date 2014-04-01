8a5252de2bc1fb565f1cefb175968cd5224d0202
Joey Kirk <joey.kirk@gmail.com>
2014-04-01 00:28:45 -0400

    First commit

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(window).scroll(function(e) {
+    var s = $(window).scrollTop(),
+        opacityVal = (s / 150.0);
+
+    $('.blur').css('opacity', opacityVal);
+});
